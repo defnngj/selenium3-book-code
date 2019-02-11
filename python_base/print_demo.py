ce5330339707b962014ff606acf9453d8d8152e1
fnngj <fnngj@126.com>
2019-02-11 23:39:41 +0800

    print demo

---
+++ python_base/print_demo.py
@@ -0,0 +1,39 @@
+
+# hello python
+print("hello python")
+
+# python打印
+name = "tom"
+age = 27
+print("name is : " + name + ", age is : " + str(age))
+
+print("name is : %s, age is : %d" %(name, age))
+
+print("name is : {}, age is : {}".format(name, age))
+
+print("name is : {1}, age is : {0}".format(age, name))
+
+print("name is : {n}, age is : {a}".format(n=name, a=age))
+
+
+# 引号与注释
+print("hello")
+print('world')
+print("你说：'早上你好'")
+print('我说："今天天气不错"')
+# print("你微笑着'向我打招呼" .')  # 错误用法
+
+
+# 单行注释
+print("hell world") # 打印hello world
+
+"""
+功能：自动化测试
+作者：虫师
+日期：2018-03-09
+"""
+
+'''
+This is a
+Multi line comment
+'''
