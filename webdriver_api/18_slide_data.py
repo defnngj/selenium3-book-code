c8a97c071fb6d5deffb826f25db1ac81a3bbf8e0
defnngj <defnngj@gmail.com>
2020-11-13 00:23:51 +0800

    更新失效的例子

--- webdriver_api/18_slide_data.py
+++ webdriver_api/18_slide_data.py
@@ -18,8 +18,25 @@ year = dwwos[0]
 month = dwwos[1]
 day = dwwos[2]
 
-action = webdriver.TouchActions(driver)
-action.scroll_from_element(year, 0, 5).perform()
-action.scroll_from_element(month, 0, 30).perform()
-action.scroll_from_element(day, 0, 30).perform()
-# ……
+# 拨动年
+action = webdriver.ActionChains(driver)
+action.click_and_hold(year).move_by_offset(0, 100).perform()
+action.reset_actions()
+
+# 拨动月
+action2 = webdriver.ActionChains(driver)
+action2.click_and_hold(month).move_by_offset(0, 150).perform()
+action2.reset_actions()
+
+# 拨动天
+action3 = webdriver.ActionChains(driver)
+action3.click_and_hold(day).move_by_offset(0, 150).perform()
+action3.reset_actions()
+
+
+# 废弃，在新的浏览器和驱动中报错
+# action = webdriver.TouchActions(driver)
+# action.scroll_from_element(year, 0, 5).perform()
+# action.scroll_from_element(month, 0, 30).perform()
+# action.scroll_from_element(day, 0, 30).perform()
+# ...
